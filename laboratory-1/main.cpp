f2c87f94024adabeb8c1838b2327e75e2a59f05c
supragt <convolk2099@gmail.com>
2024-09-09 23:57:41 +0300

    add

--- laboratory-1/main.cpp
+++ laboratory-1/main.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iostream>
 #include <clocale>
+#include <cmath>
 using namespace std;
 
 int sum(int a, int b)
@@ -7,9 +8,22 @@ int sum(int a, int b)
 	return a + b;
 }
 
+int getSeconds(int days, int hours, int minutes, int seconds) {
+	return (days * 24 * 60 * 60) + (hours * 60 * 60) + (minutes * 60) + seconds;
+};
+
+double getLength(int radius) {
+	return 2 * 3.14 * radius;
+};
+
+double getSquare(int radius) {
+	return 3.14 * pow(radius, 2);
+};
+
+
 void main() {
 	/* Задание - 1 */
-    cout << "Hello World! \n";
+	cout << "Hello World! \n";
 	setlocale(LC_ALL, "Russian");
 	cout << "Привет, мир!" << endl;
 
@@ -17,8 +31,34 @@ void main() {
 	int a = 12;
 	int b = 16;
 
-	cout << "a+b=" << a << "+" << b << "=" << sum(12, 16);
+	cout << "a+b=" << a << "+" << b << "=" << sum(12, 16) << "\n";
 
 	/* Задание - 3 */
+	int days, hours, minutes, seconds;
 
+	cout << "Введите дни, часы, минуты и секунды:";
+
+	cin >> days >> hours >> minutes >> seconds;
+
+	cout << days << " дней, " << hours << " часов, " << minutes << " минут, " << seconds << " секунд = " << getSeconds(days, hours, minutes, seconds) << " секунд\n";
+
+	/* Задание - 4 */
+	int gallons;
+
+	cout << "Введите галлоны: ";
+
+	cin >> gallons;
+
+	cout << gallons << " галлонов = " << gallons * 4 << " литров";
+
+	/* Задание - 5 */
+	int radius;
+
+	cout << "\nВведите радиус = ";
+
+	cin >> radius;
+
+	cout << "l = " << getLength(radius) << "\n";
+
+	cout << "s = " << getSquare(radius);
 }
